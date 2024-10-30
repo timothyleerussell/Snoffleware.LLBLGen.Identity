be0dbd909db9183859680bc4ab698d933dbf0846
Timothy Lee Russell <timothy@anatone.net>
2024-10-30 00:51:36 -0700

    Add additional ApplicationUser classes

---
+++ Snoffleware.LLBLGen.Identity.Core/Models/ApplicationUserLogin.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snoffleware.LLBLGen.Identity.Core.Models
+{
+    public class ApplicationUserLogin : IdentityUserLogin<string>
+    {
+        public virtual ApplicationUser User { get; set; }
+    }
+}
---
+++ Snoffleware.LLBLGen.Identity.Core/Models/ApplicationUserToken.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snoffleware.LLBLGen.Identity.Core.Models
+{
+    public class ApplicationUserToken : IdentityUserToken<string>
+    {
+        public virtual ApplicationUser User { get; set; }
+    }
+}
